3ec0442c82428ff2ee587824108e516edd2d249e
Aditya Saxena <adisaloni@yahoo.co.in>
2019-09-20 13:49:28 -0500

    Create recommend.java

---
+++ temp/recommend.java
@@ -0,0 +1,25 @@
+  public static List<String> findReachableBusinesses(Business startingBusiness, int distance) {
+		
+        List<String> l1 = new ArrayList();
+        
+        Stack<Business> stack = new Stack();
+        stack.push(startingBusiness);
+        Map<Business, Integer> distFromStart = new HashMap();
+        map.put(startingbusiness, 0);
+        
+        while(!stack.isEmpty) {
+            Business b1 = stack.pop();
+            Map<Business, Integer> map = b1.getNearbyBusinesses();
+            
+            for(Map.Entry<Business, Integer> entry : map.entrySet()) {
+               int totaldist = entry.getValue() + distFromStart.get(b1);
+               if(totaldist <= distance) {
+                   l1.add(entry.getKey().getName());
+                   stack.push(entry.getKey());
+                   distFromStart.put(entry.getKey(), totaldist);
+               }
+            }
+            
+        }
+        
+	}
