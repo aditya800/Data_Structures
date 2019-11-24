10d6b6150103f9b4a5d8aa612b286d271d2822da
Aditya Saxena <adisaloni@yahoo.co.in>
2019-11-25 00:58:17 +0530

    Update final_discounted.java

--- final_discounted.java
+++ final_discounted.java
@@ -1,12 +1,10 @@
 public static void finalPrice(List<Integer> prices) {
     
     Stack<Integer> stack = new Stack();
-    List<Integer> indices = new ArrayList();
-    int cost = prices.get(prices.size() - 1);
-    indices.add(prices.size() - 1);
-    stack.push(cost);
+        List<Integer> indices = new ArrayList();
+        long cost = 0;
 
-    for(int i = prices.size() - 2; i >= 0; i--) {
+    for(int i = prices.size() - 1; i >= 0; i--) {
         int curPrice = prices.get(i);
         while(!stack.isEmpty() && curPrice < stack.peek()) {
             stack.pop();
@@ -26,8 +24,9 @@ public static void finalPrice(List<Integer> prices) {
     for(int i : indices) {
         System.out.print(i);
         if(k++ != indices.size() - 1) {
-            System.out.println(" ");
+            System.out.print(" ");
         }
+    }
     }
 }
 
